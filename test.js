b0372258e83f8eccb007bec6268d352c75520334
CreatiCoding <33514304+CreatiCoding@users.noreply.github.com>
2018-08-27 00:00:01 +0900

    test(pandom): testing

--- test.js
+++ test.js
@@ -1,19 +1,27 @@
-var driver = require("node-phantom-simple");
+const driver = require("node-phantom-simple");
 
 driver.create({ path: require("phantomjs").path }, (err, browser) =>
   browser.createPage((err, page) =>
     page.open("http://www.woowahan.com/#/recruit/tech", (err, status) => {
-      console.log("opened site? ", status);
-      setTimeout(
-        () =>
-          page.evaluate(
-            () => $("body").html(),
-            (err, result) => {
-              console.log(result);
-              browser.exit();
-            }
-          ),
-        5000
+      page.includeJs(
+        "http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js",
+        err => {
+          console.log("opened site? ", status);
+          setTimeout(
+            () =>
+              page.evaluate(
+                function() {
+                  console.log($);
+                  return $("body").html();
+                },
+                (err, result) => {
+                  console.log(result);
+                  browser.exit();
+                }
+              ),
+            5000
+          );
+        }
       );
     })
   )
